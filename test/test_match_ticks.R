7e0acff810338a5a9cf3fc6edf8059965aeb2f50
Dmitrii Izgurskii <izgurskii@gmail.com>
2013-04-01 22:50:47 +0200

    major updates

---
+++ test/test_match_ticks.R
@@ -0,0 +1,10 @@
+source("../match_ticks.R")
+source("../find_first_common_tick.R")
+source("../lcs.R")	
+
+ticks1 <- read.csv("AUDCAD1_for_match_ticks.csv", header = TRUE)
+ticks2 <- read.csv("AUDCAD2_for_match_ticks.csv", header = TRUE)
+ticks1[, 1] <- as.POSIXct(ticks1[, 1])
+ticks2[, 1] <- as.POSIXct(ticks2[, 1])
+
+# matched_ticks <- match_ticks(ticks_list[[1]], ticks_list[[2]])
